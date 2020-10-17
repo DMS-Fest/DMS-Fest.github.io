21b2935702bdba51b6923eb11c8a30b5b10f55cc
Sanatan Shrivastava <45680718+Sanatan-Shrivastava@users.noreply.github.com>
2020-10-17 21:51:19 +0530

    added auth.js

---
+++ auth.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const volleyball = require('volleyball');
+const app = express();
+const auth = require('./Auth/index.js');
+
+
+app.use(volleyball);
+
+// Built in body-parser in express
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: '🦄🌈✨Hello World!🌈✨🦄'
+  });
+});
+
+app.use('/auth', auth);
+app.use('/auth/signup', auth);
+
+// Not found Middleware
+// next sends us to next middle ware
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error('Not Found - ' + req.originalUrl);
+  next(error);
+}
+
+// Error handling Middleware
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
